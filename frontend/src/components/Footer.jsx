9687299a192bef75fe202956d34d8351f8f3ab43
mikegongolidis <mikegongolidis@hotmail.gr>
2023-01-21 18:53:50 +0200

    feat: add footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,6 +3,8 @@
 
 export function Footer(){
     return (
-        <footer className=' absolute bottom-0 w-full bg-inherit text-center text-black dark:text-white'> This is the footer</footer>
+        <footer className=' absolute bottom-0 w-full bg-inherit text-center text-black dark:text-white'> 
+        This is the footer        
+        </footer>
     )
 }
